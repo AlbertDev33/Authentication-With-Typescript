8715a67c2a7003233e40c994bcef19c58e527ec5
AlbertDev33 <alber725@gmail.com>
2020-09-14 19:13:11 -0300

    Initial struture and code config

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+
+import express from 'express';
+
+import '../typeorm';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+  console.log('🚀 Executando na porta 3333');
+});
