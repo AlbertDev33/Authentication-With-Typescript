792253b751d7831a77930053afb94d666bb7959b
AlbertDev33 <alber725@gmail.com>
2020-09-15 19:06:43 -0300

    Added routes and typeorm configs to run the project

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,15 @@ import 'reflect-metadata';
 
 import express from 'express';
 
-import '../typeorm';
+// import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
+
+import '@shared/infra/typeorm';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Executando na porta 3333');
